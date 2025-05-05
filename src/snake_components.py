c5d401d90bc5cd6b82abbcc70999bb571e0aed12
DTiourine <109561478+DTiourine@users.noreply.github.com>
2025-05-05 17:07:48 -0400

    Works on fixing snake segment following the segment in front of it mechanic

--- src/snake_components.py
+++ src/snake_components.py
@@ -24,7 +24,18 @@ class SnakeBodySegment:
     def __init__(self, screen, next_segment=None, previous_segment=None, snake_speed: int = 5):
         if next_segment:
             self.current_direction = next_segment.get_direction()
-            self.current_segment = Rect(next_segment.current_segment.x - 20, next_segment.current_segment.y, 20, 20)
+
+            if self.current_direction == "up":
+                self.current_segment = Rect(next_segment.current_segment.x, next_segment.current_segment.y - 20, 20, 20)
+            elif self.current_direction == "down":
+                self.current_segment = Rect(next_segment.current_segment.x, next_segment.current_segment.y + 20, 20, 20)
+            elif self.current_direction == "left":
+                self.current_segment = Rect(next_segment.current_segment.x + 20, next_segment.current_segment.y, 20, 20)
+            elif self.current_direction == "right":
+                self.current_segment = Rect(next_segment.current_segment.x - 20, next_segment.current_segment.y, 20, 20)
+            else:
+                raise ValueError("Invalid direction when initializing SnakeBodySegment")
+
         else:
             self.current_direction = 'right'
             self.current_segment = Rect(screen.get_width() / 2, screen.get_height() / 2, 20, 20)
@@ -34,8 +45,8 @@ class SnakeBodySegment:
         self.next_segment = next_segment
         self.previous_segment = previous_segment
         self.snake_speed = snake_speed
-        self.next_segment_turning_point = None
-        self.next_segment_turning_point_direction = None
+        self.next_segment_turning_points = []
+        self.next_segment_turning_point_directions = []
 
     def move_segment(self):
         """Moves the segment according to the current direction """
@@ -51,9 +62,12 @@ class SnakeBodySegment:
             raise ValueError(f'Invalid direction: {self.current_direction}')
 
         # If the coordinates of the current segment match the coordinates of the turning point of the next segment
-        # then turn the current segment
-        if (self.current_segment.x, self.current_segment.y) == self.next_segment_turning_point:
-            self.update_direction(direction=self.next_segment_turning_point_direction)
+        # then turn the current segment in that same direction
+        if self.next_segment_turning_points and (self.current_segment.x, self.current_segment.y) == self.next_segment_turning_points[0]:
+            self.update_direction(direction=self.next_segment_turning_point_directions[0])
+
+            self.next_segment_turning_points.pop(0)
+            self.next_segment_turning_point_directions.pop(0)
 
         if self.previous_segment:
             self.previous_segment.move_segment()
@@ -75,8 +89,8 @@ class SnakeBodySegment:
         if direction != self.current_direction:
             if self.previous_segment:
                 # Have the previous segment remember where this segment turned
-                self.previous_segment.next_segment_turning_point = (self.current_segment.x, self.current_segment.y)
-                self.previous_segment.next_segment_turning_point_direction = direction
+                self.previous_segment.next_segment_turning_points.append((self.current_segment.x, self.current_segment.y))
+                self.previous_segment.next_segment_turning_point_directions.append(direction)
 
                 # self.previous_segment.update_direction(direction)
 
